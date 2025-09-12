ec1ae1d7a07f3c9795bcb178c5ff5a1c97cbcf5c
davidjung1001 <davidjung1001@gmail.com>
2025-09-11 23:20:35 -0500

    firxed env

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -14,7 +14,7 @@ export default function Chat() {
     setInput("")
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
